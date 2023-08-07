5d7e589f2ec5afc4b02ebd3e1a1bb2f2c242b2fe
akunnyaanam <akunnyakhoirulanam@gmail.com>
2023-08-08 01:03:41 +0700

    modeling user implement authenticatable

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Users extends Model
+class Users extends Model implements Authenticatable
 {
     use HasFactory;
     
@@ -21,4 +22,34 @@ class Users extends Model
         return $this->belongsTo(UserRules::class, 'id_rule');
     }
 
+    // Metode dari kontrak Authenticatable
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
---
+++ resources/views/dashboard/layouts/sidenav.blade.php
@@ -0,0 +1,56 @@
+@section('sidenav')
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="sb-sidenav-menu-heading">Master</div>
+                <a class="nav-link" href="/jenisBarang">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Jenis Barang
+                </a>
+                <a class="nav-link" href="/pemasok">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Pemasok
+                </a>
+                <a class="nav-link" href="/toko">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Toko
+                </a>
+                <a class="nav-link" href="/gudang">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Gudang
+                </a>
+                <a class="nav-link" href="/barang">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Barang
+                </a>
+                <div class="sb-sidenav-menu-heading">Transaksi</div>
+                <a class="nav-link" href="charts.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Pengeluaran
+                </a>
+                <a class="nav-link" href="tables.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                    Transaksi Penjualan
+                </a>
+                <div class="sb-sidenav-menu-heading">Report</div>
+                <a class="nav-link" href="charts.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Laporan Keuangan
+                </a>
+                <div class="sb-sidenav-menu-heading">Setting</div>
+                    <form action="/logout" class="nav-link" method="post">
+                        @csrf
+                        <button type="submit" class="dropdown-item">Logout</button>
+                    </form>
+                </a>
+            </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            {{ auth()->check() ? auth()->user()->name : 'Guest' }}
+            </div>
+    </nav>
+</div>
+
+@endsection
