340f567f13ffd1a1fad54c9f3e6c66780089e951
akunnyaanam <akunnyakhoirulanam@gmail.com>
2023-08-08 00:48:00 +0700

    create login page and user migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Users;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,40 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Seeding role and user
+        DB::table('user_roles')->insert([
+            'role' => 'admin'
+        ]);
+
+        DB::table('user_roles')->insert([
+            'role' => 'gudang'
+        ]);
+
+        DB::table('user_roles')->insert([
+            'role' => 'kasir'
+        ]);
+
+        \App\Models\Users::create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'id_role' => '1',
+            'password' => Hash::make('password'),
+        ]);
+
+        \App\Models\Users::create([
+            'name' => 'Pegawai Gudang',
+            'username' => 'gudang',
+            'id_role' => '2',
+            'password' => Hash::make('password'),
+        ]);
+
+        \App\Models\Users::create([
+            'name' => 'Pegawai Kasir',
+            'username' => 'kasir',
+            'id_role' => '3',
+            'password' => Hash::make('password'),
+        ]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Login 
+Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
+// Content
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
