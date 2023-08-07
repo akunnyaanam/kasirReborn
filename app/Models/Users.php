0ca12a982076c92781554520506bc46c9533351e
akunnyaanam <akunnyakhoirulanam@gmail.com>
2023-08-08 00:52:57 +0700

    modeling Users and UserRole

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Users extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function userRule(): BelongsTo
+    {
+        return $this->belongsTo(UserRules::class, 'id_rule');
+    }
+
 }
